7c591b0a2cdbfc272b864eb7da0b880d45c44557
Josh Goldberg <git@joshuakgoldberg.com>
2022-10-21 14:55:12 -0400

    Overrides in ESLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,18 @@
 module.exports = {
 	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+	overrides: [
+		{
+			files: "**/*.vue",
+			// (whatever it's called 😄)
+			parser: "@vue-eslint/parser",
+		},
+		{
+			files: "**/*.test.*",
+			rules: {
+				"@typescript-eslint/no-unsafe-call": "off",
+			},
+		},
+	],
 	parser: "@typescript-eslint/parser",
 	plugins: ["@typescript-eslint"],
 	root: true,
