61d46396fc039d97bf21510693c73b02d9c3fa5d
Josh Goldberg <git@joshuakgoldberg.com>
2022-10-21 15:10:16 -0400

    eslint-config-prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,6 +3,7 @@ module.exports = {
 		"eslint:recommended",
 		"plugin:@typescript-eslint/recommended",
 		"plugin:@typescript-eslint/recommended-requiring-type-checking",
+		"prettier",
 	],
 	overrides: [
 		{
